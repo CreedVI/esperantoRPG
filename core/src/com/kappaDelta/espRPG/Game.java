54360d920330c7b1f3be2cd8d7643c4ab11a7574
Calvin Day <cdayvi@gmail.com>
2015-07-17 00:06:02 -0700

    /
    /

--- core/src/com/kappaDelta/espRPG/Game.java
+++ core/src/com/kappaDelta/espRPG/Game.java
@@ -3,28 +3,26 @@ package com.kappaDelta.espRPG;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Game extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-        Player pco;
-        
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-                pco = new Player();
-	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+    float halfW;
+    float halfH;
+    
+    @Override
+    public void create() {
+        halfW = Gdx.graphics.getWidth()/2;
+        halfH = Gdx.graphics.getHeight()/2;
+        Assets.load();
+
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Assets.batch.begin();
+        Assets.batch.draw(Assets.charSprite, halfW-16, halfH-16);
+        Assets.batch.end();
+    }
 }
